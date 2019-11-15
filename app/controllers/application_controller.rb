698a5b5e7d531a39c501de825c73b14bde47948c
Andrew Silva <andrewsilva9@gmail.com>
2019-11-15 01:42:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   
   post "/articles" do 
     Article.create(params)
-    redirect "/articles/#{id}"
+    redirect "/articles/#{articles.id}"
   end 
   
   get "/articles" do
